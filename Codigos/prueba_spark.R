e3e02ea176f3713b11629dcaaf37dcfd8d542c41
Franco-Santini <francosantini222@gmail.com>
2025-07-24 16:34:05 -0300

    Avance del trabajo, unimos tablas y filtramos tiendas

--- Codigos/prueba_spark.R
+++ Codigos/prueba_spark.R
@@ -2,7 +2,7 @@ library(sparklyr)
 library(dplyr)
 library(ggplot2)
 library(dbplot)
-library(MLib)
+library(lubridate)
 
 # Establecemos la coneccion con spark
 sc <- spark_connect(master = "local")
@@ -28,6 +28,100 @@ eci_product_groups <- spark_read_csv(sc, name = "eci_grupo_productos", path = "D
 eci_product_master <- spark_read_csv(sc, name = "eci_maestros_productos", path = "Datos/eci_product_master.csv", columns = product_master_spec)
 eci_stores <- spark_read_csv(sc, name = "eci_tiendas", path = "Datos/eci_stores.csv", columns = stores_spec)
 eci_stores_clusters <- spark_read_csv(sc, name = "eci_tiendas_cluster", path = "Datos/eci_stores_clusters.csv", columns = stores_cluster_spec)
+eci_stores_totales <- eci_stores |> collect() |> mutate(CLOSEDATE = as_date(CLOSEDATE)) |> filter(CLOSEDATE > "2023-12-31" | is.na(CLOSEDATE)) |> filter(!is.na(STORE_TYPE))
+eci_stores_clusters_totales <- eci_stores_clusters |> collect()
+eci_product_master_totales <- eci_product_master |> collect() 
+
+# Renombre de las columnas
+# Tabla de tiendas
+eci_stores_clusters_join <- eci_stores_totales |> 
+  left_join(eci_stores_clusters_totales, by = "STORE_ID") |> 
+  select(-c(BRAND.y, STORE_NAME.y)) |> 
+  rename(BRAND = BRAND.x, STORE_NAME = STORE_NAME.x)
+
+# Tabla del maestro de productos
+eci_product_master_totales <- eci_product_master_totales |> rename(SKU = sku, SUBGROUP = subgroup)
+
+# Filtrar los datos faltantes 
+
+# Intento unir las bases de transacciones y stores
+eci_transactions_stores_sample <- eci_transactions_sample |> 
+  left_join(eci_stores |> collect(), by = "STORE_ID") |> 
+  mutate(DATE = as_date(DATE))
+
+eci_transactions_stores_sample$mes <- month(eci_transactions_stores_sample$DATE)
+eci_transactions_stores_sample$año <- year(eci_transactions_stores_sample$DATE)
+eci_transactions_stores_sample$mes_año <- ym(paste(eci_transactions_stores_sample$año, eci_transactions_stores_sample$mes, sep = "/"))
+
+# Join de transacciones con productos
+eci_transactions_stores_prod_sample <- eci_transactions_stores_sample |> 
+  left_join(eci_product_master_totales, by = c("SKU", "SUBGROUP"))
+
+
+# Trabajamos con una muestra de todos los datos
+eci_transactions_sample <- eci_transactions |> 
+  sdf_sample(0.02) |> # Trabajamos con solo el 2% de los datos 
+  collect()
+
+# Deteccion de valores faltantes (Todos los data frames)
+# Muestra de la base de datos de transacciones
+eci_transactions_sample |> 
+  select(everything()) |> 
+  summarize(across(everything(), ~sum(is.na(.))))
+
+# Base completa de grupo de productos
+eci_product_groups |> 
+  select(everything()) |> 
+  collect() |>
+  summarize(across(everything(), ~sum(is.na(.))))
+
+# Base completa del maestro de productos
+eci_product_master |> 
+  select(everything()) |> 
+  collect() |> 
+  summarize(across(everything(), ~sum(is.na(.))))
+
+# Base completa de las tiendas
+eci_stores |> 
+  select(everything()) |> 
+  collect() |> 
+  summarize(across(everything(), ~sum(is.na(.))))
+
+# Base completa de los clusters de las tiendas
+eci_stores_clusters |> 
+  select(everything()) |> 
+  collect() |> 
+  summarize(across(everything(), ~sum(is.na(.))))
+
+# Base completa de clientes (ni idea si se tiene que usar)
+eci_customer |> 
+  select(everything()) |> 
+  collect() |> 
+  summarize(across(everything(), ~sum(is.na(.))))
+
+# Evolucion temporal de la demanda
+eci_transactions_stores_sample |> 
+  group_by(mes_año) |> 
+  summarise(demanda = sum(TOTAL_SALES)) |> 
+  ggplot() +
+  aes(x = mes_año, y = demanda) +
+  geom_line() +
+  geom_point() +
+  theme_bw() +
+  theme(legend.position = "top")
+
+# Evolucion temporal de la demanda x subgrupos que más demanda tienen
+eci_transactions_stores_sample |> 
+  group_by(mes_año, SUBGROUP) |> 
+  summarise(demanda_x_subg = sum(TOTAL_SALES)) |> 
+  filter(SUBGROUP %in% c("Headphones", "Wearables", "Speakers")) |> 
+  ggplot() +
+  aes(x = mes_año, y = demanda_x_subg, color = factor(SUBGROUP)) +
+  geom_line() +
+  geom_point() +
+  theme_bw() +
+  theme(legend.position = "top")
+
 
 # Analisis exploratorio (dividido en dos partes, agrupar los datos trabajando en spark y luego graficar)
 
@@ -45,6 +139,12 @@ transacciones_agrup |>
   coord_flip() +
   theme_bw()
 
+transacciones_agrup_store_sample <- eci_transactions_stores_sample |> 
+  group_by(STORE_ID, SUBGROUP) |> 
+  summarise(Demanda = sum(TOTAL_SALES)) |> 
+  ungroup() |> 
+  collect() |> 
+  print()
 
 # Desconectamos spark
 spark_disconnect(sc)
